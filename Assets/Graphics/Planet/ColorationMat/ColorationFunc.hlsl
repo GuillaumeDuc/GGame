58c5471941a526518ed5c4e83f16b888972b606e
GuillaumeDuc <ducoeurguillaume@gmail.com>
2023-09-23 13:15:37 +0200

    new noise

--- Assets/Graphics/Planet/ColorationMat/ColorationFunc.hlsl
+++ Assets/Graphics/Planet/ColorationMat/ColorationFunc.hlsl
@@ -11,25 +11,6 @@ float4 getOcean (float noise) {
   return oc;
 }
 
-float4 getRegions (float3 position, float regionScale, float baseNoise, float details, float mountains) {
-  float maskNoise = clamp(baseNoise, -1, 1);
-  float region1Noise = clamp(snoise((position + 5) * regionScale), -1, 1);
-  float region2Noise = clamp(snoise((position + 7) * regionScale), -1, 1);
-  region1Noise += region1Noise <= 0 ? 0 : details;
-  region2Noise += region2Noise <= 0 ? 0 : details;
-  float totalNoises = region1Noise + region2Noise;
-
-  if (region1Noise > 0 && region2Noise > 0 && maskNoise > 0) {
-    return clamp(lerp(getGradient(region1Noise, 1), getGradient(region2Noise, 2), region2Noise), 0, 1) * (1 - mountains);
-  }
-  else if (region1Noise > 0 && maskNoise > 0) {
-    return getGradient(region1Noise, 1) * (1 - mountains);
-  } else if (region2Noise > 0 && maskNoise > 0) {
-    return getGradient(region2Noise, 2) * (1 - mountains);
-  }
-  return getOcean(maskNoise);
-}
-
 float evaluate(float3 position, int numLayers, float strength, float baseRoughness, float roughness, float persistence, float minValue) {
   float noiseValue = 0;
   float frequency = baseRoughness;
@@ -46,6 +27,28 @@ float evaluate(float3 position, int numLayers, float strength, float baseRoughne
   return noiseValue * strength;
 }
 
+float4 getRegions (float3 position, float regionScale, float baseNoise, float details, float mountains) {
+  float maskNoise = clamp(baseNoise, -1, 1);
+  
+  // float region1Noise = clamp(snoise((position + 5) * regionScale), -1, 1);
+  // float region2Noise = clamp(snoise((position + 7) * regionScale), -1, 1);
+  float region1Noise = clamp(evaluate(position + 5, 5, 3.5, regionScale, 5, .15f, .5f), -1, 1);
+  float region2Noise = clamp(evaluate(position + 20, 5, 3.5, regionScale, 5, .15f, .5f), -1, 1);
+  region1Noise += region1Noise <= 0 ? 0 : details;
+  region2Noise += region2Noise <= 0 ? 0 : details;
+  float totalNoises = region1Noise + region2Noise;
+
+  if (region1Noise > 0 && region2Noise > 0 && maskNoise > 0) {
+    return clamp(lerp(getGradient(region1Noise, 1), getGradient(region2Noise, 2), region2Noise), 0, 1) * (1 - mountains);
+  }
+  else if (region1Noise > 0 && maskNoise > 0) {
+    return getGradient(region1Noise, 1) * (1 - mountains);
+  } else if (region2Noise > 0 && maskNoise > 0) {
+    return getGradient(region2Noise, 2) * (1 - mountains);
+  }
+  return getOcean(maskNoise);
+}
+
 float getMountains (float3 position, float mountainScale, float baseNoise) {
   float mountains = (1 - abs(cnoise((position + 10) * mountainScale))) - (1 - baseNoise);
   return clamp(mountains, 0, 1) / 2;
@@ -59,21 +62,22 @@ float getDetails (float3 position, float detailScale, float baseNoise) {
 
 void ColorationFunc_float (
   float3 position,
-  float scale,
-  float baseStrength,
+  int numLayers,
+  float strength,
+  float baseRoughness,
+  float roughness,
+  float persistence,
+  float minValue,
   float regionScale,
   float mountainScale,
   float detailScale,
   out float4 color,
-  out float3 smooth,
-  out float4 outEv
+  out float3 smooth
   ) {
-  // float testnoise = evaluate(position, 5, .5f, 5, 5, 0.6f, 1.3f);
-  // outEv = getGradient(testnoise, 1);
-  float baseNoise = cnoise(position * scale) + baseStrength;
-  float mountains = getMountains(position, mountainScale, baseNoise);
-  float details = getDetails(position, detailScale, baseNoise);
-  float4 regions = getRegions(position, regionScale, baseNoise, details, mountains);
+  float baseNoise = clamp(evaluate(position, numLayers, strength, baseRoughness, roughness, persistence, minValue), -1, 1);
+  // float mountains = getMountains(position, mountainScale, baseNoise);
+  // float details = getDetails(position, detailScale, baseNoise);
+  float4 regions = getRegions(position, regionScale, baseNoise, 0, 0);
 
   color = regions;
   smooth = 1 - baseNoise;
